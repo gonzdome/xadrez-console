1d20ad0e353b6ac7e247b8cda1392dc7d44a3608
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2024-11-18 22:21:06 -0300

    feat: add class Screen

---
+++ xadrez-console/screen/Screen.cs
@@ -0,0 +1,22 @@
+﻿using board;
+
+namespace screen;
+
+public class Screen
+{
+    public static void printBoard(Board board)
+    {
+        for (int i = 0; i < board.rows; i++)
+        {
+            for (int j = 0; j < board.columns; j++) 
+            {
+                if (board.piece(i, j) == null) 
+                    Console.Write("- ");
+                else
+                    Console.Write(board.piece(i, j));
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
